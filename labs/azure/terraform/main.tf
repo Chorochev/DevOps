378662728ad2dc636f687358c486ad8900a687d2
chorochev <chorochev@ya.ru>
2023-01-17 13:36:58 +0600

    Add readme
    debug - create resource group

---
+++ labs/azure/terraform/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+
+  subscription_id = ""
+  tenant_id       = ""
+}
+
+resource "azurerm_resource_group" "name" {
+  name     = "rg-lab-qa-eastasia-001"
+  location = "East Asia"
+}
