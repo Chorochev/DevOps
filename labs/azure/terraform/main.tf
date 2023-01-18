1161b4d314fb6e40d6d7cdbaf7cd898b31e4f50f
chorochev <chorochev@ya.ru>
2023-01-18 13:21:28 +0600

    add depends

--- labs/azure/terraform/main.tf
+++ labs/azure/terraform/main.tf
@@ -15,20 +15,24 @@ provider "azurerm" {
   subscription_id = var.subscription_id
 }
 
-resource "azurerm_resource_group" "qa" {
+resource "azurerm_resource_group" "main" {
   name     = var.qa_resource_group_name
   location = var.location
 }
 
-resource "azurerm_virtual_network" "qa" {
+resource "azurerm_virtual_network" "main" {
   name                = var.qa_virtual_network_name
-  location            = azurerm_resource_group.qa.location
-  resource_group_name = azurerm_resource_group.qa.name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
   address_space       = ["10.0.0.0/16"]
+
+  depends_on = [azurerm_resource_group.main]
 }
 
-resource "azurerm_network_watcher" "qa" {
+resource "azurerm_network_watcher" "main" {
   name                = var.qa_network_watcher_name
-  location            = azurerm_resource_group.qa.location
-  resource_group_name = azurerm_resource_group.qa.name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  depends_on = [azurerm_resource_group.main]
 }
