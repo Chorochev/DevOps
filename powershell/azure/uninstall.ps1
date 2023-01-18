4addcc957042951a96195d323bbc61227cdb35bd
chorochev <chorochev@ya.ru>
2023-01-18 13:40:02 +0600

    uninstall Azure resourses

---
+++ powershell/azure/uninstall.ps1
@@ -0,0 +1,8 @@
+# connect to Azure
+Connect-AzAccount
+
+# Deleting all resources
+Get-AzResourceGroup | ForEach-Object {     
+    Write-Host "`nDeleting the resource group '"$_.ResourceGroupName"'!!!`n" -ForegroundColor Red 
+    Remove-AzResourceGroup -Name $_.ResourceGroupName     
+}
