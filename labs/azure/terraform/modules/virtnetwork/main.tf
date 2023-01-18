ccd400cf011ef2c1e36f1b32b9d2ec539c381adb
chorochev <chorochev@ya.ru>
2023-01-18 18:10:22 +0600

    delete Network Watcher

--- labs/azure/terraform/modules/virtnetwork/main.tf
+++ labs/azure/terraform/modules/virtnetwork/main.tf
@@ -1,21 +1,19 @@
+////////////////////
+// Resource group
+////////////////////
 resource "azurerm_resource_group" "main" {
   name     = "rg-${var.workload}-${var.environment}-${var.location}-${var.instance}"
   location = var.location
 }
 
+////////////////////
+// Virtual network
+////////////////////
 resource "azurerm_virtual_network" "main" {
   name                = "net-${var.workload}-${var.environment}-${var.location}-${var.instance}"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
   address_space       = var.address_space
   
-  depends_on = [azurerm_resource_group.main, azurerm_network_watcher.main]
-}
-
-resource "azurerm_network_watcher" "main" {
-  name                = "netw-${var.workload}-${var.environment}-${var.location}-${var.instance}"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-
   depends_on = [azurerm_resource_group.main]
 }
--- labs/azure/terraform/modules/virtnetwork/variable.tf
+++ labs/azure/terraform/modules/virtnetwork/variable.tf
@@ -1,9 +1,6 @@
+//////////////
 // Variables
-variable "subscription_id" {
-  type        = string
-  default     = "00000000-0000-0000-0000-000000000000"
-  description = "Subscription id"
-}
+//////////////
 
 variable "location" {
   type        = string  
