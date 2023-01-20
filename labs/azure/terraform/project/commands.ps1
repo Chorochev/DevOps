64bbca9a09a32e927c4573e0a91947f19ff60e0d
chorochev <chorochev@ya.ru>
2023-01-20 16:41:58 +0600

    workspace

--- labs/azure/terraform/project/commands.ps1
+++ labs/azure/terraform/project/commands.ps1
@@ -2,12 +2,15 @@ terraform init
 
 #####################################
 # QA section
+terraform workspace new qa_workspace
 terraform plan -var-file="qa.tfvars"
-
 terraform apply -var-file="qa.tfvars"
-
+terraform workspace select qa_workspace
+terraform workspace delete qa_workspace
 #####################################
 # PROD section
+terraform workspace new prod_workspace
 terraform plan -var-file="prod.tfvars"
-
 terraform apply -var-file="prod.tfvars"
+terraform workspace select prod_workspace
+terraform workspace delete prod_workspace
