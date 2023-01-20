13e563574a72d1234b126944beecd1eefa354ba0
chorochev <chorochev@ya.ru>
2023-01-20 20:43:05 +0600

    output

--- labs/azure/terraform/modules/virtnetwork/outputs.tf
+++ labs/azure/terraform/modules/virtnetwork/outputs.tf
@@ -1,11 +1,14 @@
 output "resource_group" {
-  value = azurerm_resource_group.this.name
-}
-
-output "virtual_network" {
-  value = azurerm_virtual_network.this.name
+  value       = azurerm_resource_group.this.name
+  description = "The resource group"
 }
 
 output "network_watcher" {
-  value = azurerm_network_watcher.this.name
+  value       = azurerm_network_watcher.this.name
+  description = "The network watcher"
+}
+
+output "virtual_network" {
+  value       = azurerm_virtual_network.this.name
+  description = "The virtual_network"
 }
